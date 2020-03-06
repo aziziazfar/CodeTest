d8b4dc895d45c7f7cfacb92e74a32a8d6b326cb0
aziziazfar <aziziazfar95@gmail.com>
2020-03-07 03:18:02 +0800

    Added files to repo

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class CubeController : MonoBehaviour {
+
+	public GameObject cube;
+	public Transform intitialPosition; //Point A
+	public Transform targetPosition; // Point B
+	public float AnimationDelay;
+    public float AnimationDuration;
+    public float CubeLifetime;
+
+	public int initialNumCubes = 0;
+	public static int numCubes;
+
+	public Text numUI;
+
+
+
+	[System.Serializable]
+	public class Item
+    {
+        public float AnimationDelay;
+        public float AnimationDuration;
+        public float CubeLifetime;
+
+		
+		public static Item CreateFromJSON(string jsonString)
+		{
+			return JsonUtility.FromJson<Item>(jsonString);
+		}
+	}
+	private void Awake() 
+    {
+        numUI.text = initialNumCubes.ToString();
+    }
+
+	// Use this for initialization
+	void Start () {
+		// perform Load from data.json file
+		using (StreamReader r = new StreamReader("./Assets/Data.json"))
+        {
+            string json = r.ReadToEnd();
+			Item item = Item.CreateFromJSON (json);
+			AnimationDelay = item.AnimationDelay;
+			AnimationDuration = item.AnimationDuration;
+			CubeLifetime = item.CubeLifetime;
+		}
+	}
+	public static void UpdateScore(int newScore) 
+    {
+        Debug.Log("Score Updated: Added " + newScore + " points!");
+        numCubes += newScore;
+    }
+
+    void LateUpdate()
+    {
+        numUI.text = initialNumCubes.ToString();
+
+        initialNumCubes = numCubes;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		 
+		if(Input.GetKeyDown(KeyCode.Return))
+         {
+			UpdateScore(1);
+            Debug.Log("Cube Appears");
+			NewCube();
+     	}
+	}
+
+	private void NewCube()
+	{
+
+			Debug.Log(CubeLifetime);
+
+			Vector3 cubePos = new Vector3(-5, 0, 5);
+			Quaternion cubeRot = new Quaternion(0,0,0,0);
+			GameObject newCube = Instantiate(cube, cubePos, cubeRot);
+			
+			// buggy
+			// need to move into Update()
+			// newCube.transform.position = Vector3.MoveTowards(intitialPosition.position, targetPosition.position, AnimationDuration);
+
+			Destroy(newCube, CubeLifetime);
+	}
+}
---
+++ Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 endPos = new Vector3(5,0,0);
+		gameObject.transform.position = Vector3.MoveTowards(
+			gameObject.transform.position,
+			endPos,
+			1.0f * 0.1f // movement speed hardcoded 
+			);
+	}
+}
